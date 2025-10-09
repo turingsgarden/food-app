a371d0aebb639a9fca1d56b6b12b06d88a329b67
He(Helen) Tu <167085606+ht2604@users.noreply.github.com>
2025-10-09 14:44:41 -0400

    Update app.py

--- model-answer-display/app.py
+++ model-answer-display/app.py
@@ -52,7 +52,7 @@ def load_model_data():
     return model_data, available_models
 
 def get_all_images():
-    image_dir = "food101-100images"
+    image_dir = "food-101_100images"
     image_files = glob.glob(os.path.join(image_dir, "*.jpg"))
     if not image_files:
         return None
