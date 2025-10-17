939fabcc44f7dfa1b5e20268fe4980b109386df8
He(Helen) Tu <167085606+ht2604@users.noreply.github.com>
2025-10-16 20:42:05 -0400

    Remove title display from model answer page
    Remove title display for the model answer section.

--- model-answer-display/display_main.py
+++ model-answer-display/display_main.py
@@ -5,9 +5,6 @@ from version1 import run_version1
 # 初始化 session_state
 if "selected_version" not in st.session_state:
     st.session_state.selected_version = None
-
-st.title("Model Answer Display")
-
 # 如果还没选择版本，则显示选择框
 if st.session_state.selected_version is None:
     page = st.selectbox("Select version", ["Version 1", "Version 2"])
